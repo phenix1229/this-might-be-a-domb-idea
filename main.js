53c03395855de370e4d5ceb33616e4291c4d86ac
phenix1229 <paul.garay@codeimmersives.com>
2019-10-08 13:25:55 -0400

    half done

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-1
+const pgraph = document.querySelector('#p1');
+pgraph.style.color = 'lightBlue';
+
+const heading = document.querySelector('h1');
+heading.style.fontSize = '10px';
+
+pgraph.innerText = 'Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.';
+
+// const item13 = document.querySelector('#item13');
+// item13.style.
+
+const item3 = document.querySelector('#item-3');
+item3.innerText = 'I say, "Hi!"';
